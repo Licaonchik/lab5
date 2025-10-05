56ea8d21c9a9c1a8647c58393b0c4e0513494da3
Licaonchik <ereneger1609@gmail.com>
2025-10-05 11:55:18 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Введите а");
+ double a = Convert.ToDouble(Console.ReadLine());
+  Console.WriteLine("Введите b");
+   double b = Convert.ToDouble(Console.ReadLine());
+    Console.WriteLine("Введите c");
+     double c = Convert.ToDouble(Console.ReadLine());
+      Console.WriteLine("Введите d");
+       double d = Convert.ToDouble(Console.ReadLine());
+        double s = 0;
+         if (a <= b && b <= c && c <= d)
+          {
+           for (double i = c; i >= c && i <= d; i += 2)
+            {
+             s += i;
+
+              }
+               Console.WriteLine(s);
+                }
+                 else
+                  {
+                   Console.WriteLine("Некорректные числа");
+                        }
